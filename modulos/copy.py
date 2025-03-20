e401b561276058e4f7f2ee4dcdfcba8c62c923e6
Gabriel <gabrielgv.16a@gmail.com>
2025-03-20 11:31:30 -0300

    new file:   modulos/copy.py

---
+++ modulos/copy.py
@@ -0,0 +1,45 @@
+import shutil,os
+
+proyecto="COT-21312 A.R.G. TRAILER-V01"
+ext =".pdf"
+origen ="C:\\app_py\\AppZero\\pdf\\"
+destino = "C:\\app_py\\AppZero\\unir\\"
+os.makedirs(origen,exist_ok=True)
+os.makedirs(destino,exist_ok=True)
+
+
+#copia = [1,27,28,29,30,31,32,41,42,43,44,66,84,85,116]
+copia = []
+
+cantidad = int(input("Cuantos archivos va a copiar:"))
+cantidad +=1
+
+for i in range(1,cantidad):
+    v = 0
+    app=""
+    v = int(input("Ingrese pag. a copiar:"))
+    
+    if v < 10 :
+        app = f"{proyecto}-0{str(v)}{ext}"        
+    else:
+        app = f"{proyecto}-{str(v)}{ext}"  
+    
+    copia.append(app) 
+
+n=0
+for archivo in copia:
+
+    archivo_origen = os.path.join(origen, archivo)
+    archivo_destino = os.path.join(destino, archivo)     
+
+    if os.path.exists(archivo_origen):
+
+        shutil.copy(archivo_origen,archivo_destino)
+        print(f"{archivo}' copiado")
+        n+=1
+
+    else:
+
+        print(f"{archivo} no se encuentra")
+
+print(f"Total de {n} archivos copiados")
